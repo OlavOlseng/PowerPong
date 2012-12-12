748288ba8adf6e2287e041b8b2a7a618d0e68696
eirikjak <eirikjak@gmail.com>
2012-12-13 00:35:24 +0100

    Adds an abstract game class, and an implementation of it.

--- src/PowerPong.cpp
+++ src/PowerPong.cpp
@@ -8,32 +8,14 @@
 #include <GL\freeglut.h>
 #include <glm\glm.hpp>
 #include "eirik.h"
+#include "Game.h"
 
-void display(){
-	glm::vec3 vec(1,1,1);
-	printf("%f",vec.x);
-	glClearColor(1.0,0,0,0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
-
-
-}
 
 
 int main(int argc, char* argv[])
 {
-	glutInit( &argc, argv);
-	glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE);
-	glutInitWindowSize(1280,720);
-	glutCreateWindow("test");
-	glutDisplayFunc(display);
-
-	glutMainLoop();
-
-	Eirik j;
-
-	std::cout << "VAGINA!" << std::endl;
-	system("PAUSE");
+	Game game(argc,argv);
+	
 	return 0;
 }
 
