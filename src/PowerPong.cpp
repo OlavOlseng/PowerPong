4148e755067b1d305546c356a6cafb888c3068bf
OlavOlseng <olseng.o@gmail.com>
2012-12-12 17:29:08 +0100

    initial commit with project files

---
+++ src/PowerPong.cpp
@@ -0,0 +1,14 @@
+// PowerPong.cpp : Defines the entry point for the console application.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char** argv[])
+{
+	std::cout << "VAGINA!" << std::endl;
+	system("PAUSE");
+	return 0;
+}
+
