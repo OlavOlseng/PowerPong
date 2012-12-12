cb178d2245f629f314e0abd4872944b1fbe05420
OlavOlseng <olseng.o@gmail.com>
2012-12-12 18:59:43 +0100

    Added projectfiles and setup initial libs, check folder path

--- src/PowerPong.cpp
+++ src/PowerPong.cpp
@@ -4,9 +4,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <glm\glm.hpp>
 
-int main(int argc, char** argv[])
+void display(){
+	glm::vec3 vec(1,1,1);
+	printf("%f",vec.x);
+	glClearColor(1.0,0,0,0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+
+
+}
+int main(int argc, char* argv[])
 {
+	glutInit( &argc, argv);
+	glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE);
+	glutInitWindowSize(600,400),
+	glutCreateWindow("test");
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+
 	std::cout << "VAGINA!" << std::endl;
 	system("PAUSE");
 	return 0;
